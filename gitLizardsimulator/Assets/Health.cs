7871713f549fd4b18d11f95541f0380444efe60f
guardmeister <linus@peterjonsson.nu>
2018-11-05 18:40:09 +0100

    shootingUpdate

--- gitLizardsimulator/Assets/Health.cs
+++ gitLizardsimulator/Assets/Health.cs
@@ -1,25 +1,72 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
 
     public const int maxHealth = 100;
+    public bool destroyOnDeath;
+
+    [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
+
     public RectTransform healthBar;
+
+    private NetworkStartPosition[] spawnPoints;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
 
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+            return;
+
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
-            currentHealth = 0;
-            Debug.Log("Dead!");
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                currentHealth = maxHealth;
+
+                // called on the Server, invoked on the Clients
+                RpcRespawn();
+            }
         }
+    }
 
-        healthBar.sizeDelta = new Vector2(
-            currentHealth,
-            healthBar.sizeDelta.y);
+    void OnChangeHealth(int currentHealth)
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
 
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // Set the spawn point to origin as a default value
+            Vector3 spawnPoint = Vector3.zero;
+
+            // If there is a spawn point array and the array is not empty, pick one at random
+            if (spawnPoints != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+
+            // Set the player’s position to the chosen spawn point
+            transform.position = spawnPoint;
+        }
     }
 }
